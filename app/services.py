f71c57776b9ff5426f9381c70bb6bb98114f66c3
simonari <vsimonari@gmail.com>
2023-11-15 14:30:53 +0700

    Fixed error with adding existing entries

--- app/services.py
+++ app/services.py
@@ -21,8 +21,20 @@ class DataBaseManger:
         async with self.engine.begin() as conn:
             return await conn.run_sync(models.Base.metadata.create_all)
 
-    async def add_data(self, data: ct.response):
-        to_add = [models.Vacancy(**item) for item in data]
+    async def add_data(self, data: ct.response) -> int:
+        missing = []
+
+        for item in data:
+            query = select(models.Vacancy).where(models.Vacancy.id == item["id"])
+            result = await self.session.execute(query)
+
+            if result is not None:
+                continue
+
+            missing.append(item)
+
+        to_add = [models.Vacancy(**item) for item in missing]
 
         self.session.add_all(to_add)
         await self.session.commit()
+        return len(to_add)
